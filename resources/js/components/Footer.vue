08ab4392ac6c910ce46a214058e4909b2bf934ba
Zakir ahmed <ahmedjakir257@gmail.com>
2020-10-28 01:06:37 +0600

    secound

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,9 +1,8 @@
 <template>
 	<!-- footer -->
 	<footer class="border-t mt-3 pt-3 py-3 px-4 lg:px-0">
-		<div class="py-4">
-			<div class="text">
-				© Copyright 2020. All Rights Reserved Fighter.
+		<div class="container mx-auto py-4">
+			<span class="text"> All Rights Reserved Fighter.
 				<span>Developed by 
 					<span>
 						<a href="https://www.facebook.com/zakir.ahmed.7393264" target="_blank"> 
@@ -11,26 +10,22 @@
 						</a>
 					</span>
 				</span>
-			</div>
-			<div class="icon">
-					<a href="https://www.facebook.com/Fighter-101743478403033/?modal=admin_todo_tour" target="blank">
-						<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="facebook svg-inline--fa fa-facebook-f fa-w-10 fa-3x"><path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" class=""></path></svg>
-					</a>
-					<a href="">
-						<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="youtube svg-inline--fa fa-youtube fa-w-18 fa-3x"><path fill="currentColor" d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" class=""></path></svg>
-					</a>
-			</div>
+			</span>
+			<span class="icon">
+				<a href="https://www.facebook.com/Fighterbd-109903737574486/?modal=admin_todo_tour" target="blank">
+					<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="facebook svg-inline--fa fa-facebook-f fa-w-10 fa-3x"><path fill="currentColor" d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" class=""></path></svg>
+				</a>
+				<a href="https://www.youtube.com/channel/UC_mQUxV6VBk5ABhx5AZCYIQ?view_as=subscriber"  target="blank">
+					<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="youtube svg-inline--fa fa-youtube fa-w-18 fa-3x"><path fill="currentColor" d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" class=""></path></svg>
+				</a>
+			</span>
 		</div>
 	</footer>
 </template>
 
 
 <style scoped>
-.text{
-	
-    margin-left: 304px;
 
-}
 
 .facebook{
 	
@@ -56,8 +51,40 @@
 	cursor: pointer;
 
 }
-.icon{
-	margin-left: 1351px;
-    margin-top: -27px;
+
+@media (max-width: 3000px) {
+
+	.icon{
+		margin-left: 800px;
+	}
+
 }
+
+@media (max-width: 1200px) {
+	.icon{
+		margin-left: 362px;
+		position: absolute;
+	}
+
+}
+
+@media (max-width: 850px) {
+	.icon{
+		margin-left: 165px;
+		position: absolute;
+	}
+
+}
+
+@media (max-width: 650px) {
+	.icon{
+		margin-left: -217px;
+		margin-top: 41px;
+		position: absolute;
+	}
+
+}
+
+
 </style>
+
