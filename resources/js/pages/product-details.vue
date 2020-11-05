68043d17bbb3690ad59aa8c3376f66ed3f1a10a3
Zakir ahmed <ahmedjakir257@gmail.com>
2020-11-06 03:58:16 +0600

    baro

--- resources/js/pages/product-details.vue
+++ resources/js/pages/product-details.vue
@@ -514,7 +514,7 @@
 									<input
 										v-model="number"
 										class="appearance-none mt-2 block w-full bg-gray-200 text-gray-700 border border-gray-600 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-										placeholder="Sender number"
+										placeholder="Contact number"
 										type="number"
 									/>
 								</div>
